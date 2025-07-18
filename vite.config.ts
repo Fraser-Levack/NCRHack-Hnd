832f0905badf6c78e2732f0ec0a0d592958840b4
Fraser Levack <flevack28@gmaiml.com>
2025-07-18 21:07:07 +0100

    you need to get int the folder

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,18 @@ export default defineConfig({
       'ira-defined-reform-logged.trycloudflare.com',
       'lawn-bishop-animal-sleeping.trycloudflare.com'
     ]
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        txMenu: 'public/txMenu/txMenu.html',
+        cashDeposit: 'public/cashDeposit/cashDeposit.html',
+        withdrawal: 'public/withdrawal/withdrawal.html',
+        balance: 'public/balance/balance.html',
+        done: 'public/done/done.html',
+        withdrawSuccess: 'public/withdrawSuccess/withdrawSuccess.html'
+      }
+    }
   }
 })
